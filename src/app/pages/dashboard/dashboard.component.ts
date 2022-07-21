c66c3cca94c210f5acc2061dd1c422cc9c73b236
Evistix <mephistakaiwan@gmail.com>
2022-07-21 18:19:53 +0200

    optimizing code for filtering

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,9 +6,9 @@ import {AlertService} from '../../services/alert.service';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {GroupRoom} from '../../domain/GroupRoom';
 import {GroupRoomService} from '../../services/group-room.service';
-import {UserService} from "../../services/user.service";
-import {User} from "../../domain/User";
-import {AuthService} from "../../services/auth.service";
+import {UserService} from '../../services/user.service';
+import {User} from '../../domain/User';
+import {AuthService} from '../../services/auth.service';
 import {NavbarComponent} from '../../components/navbar/navbar.component';
 import {CategoryService} from '../../services/categoryService';
 import {GameDTO} from '../../domain/dto/GameDTO';
@@ -18,17 +18,17 @@ import {Role} from '../../domain/Role';
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'dashboard.component.html',
-  styleUrls:['/dashboard.css']
+  styleUrls: ['/dashboard.css']
 })
 export class DashboardComponent implements OnInit {
 
   public data: any;
   public isAdmin = false;
-  public currentUser:User;
-  public chosenGame:GameDTO;
+  public currentUser: User;
+  public chosenGame: GameDTO;
   public display = 'none';
-  public chosenRole:Role;
-  public chosenCategory:Category;
+  public chosenRole: Role;
+  public chosenCategory: Category;
   public groupRooms: GroupRoom[] = [];
 
   constructor(private groupRoomService: GroupRoomService,
@@ -36,39 +36,39 @@ export class DashboardComponent implements OnInit {
               private formBuilder: FormBuilder,
               private alertService: AlertService,
               private router: Router,
-              private userService:UserService,
-              private authService:AuthService,
-              private categoryService:CategoryService) {
+              private userService: UserService,
+              private authService: AuthService,
+              private categoryService: CategoryService) {
   }
 
   ngOnInit() {
     this.chosenGame = this.categoryService.getGame();
-    console.log(this.chosenGame?.name)
-    console.log(this.chosenGame?.inGameRoles)
     this.reloadGame()
-    if(this.checkIfLoggedIn()) {
+    if (this.checkIfLoggedIn()) {
       this.checkIfAdmin()
     }
   }
+
   loadData() {
     this.groupRoomService.getGroups().subscribe(
-      (data:any) => this.groupRooms = data,
+      (data: any) => this.groupRooms = data,
       () => this.alertService.error('Error while getting groups')
     );
   }
-  joinGroup(groupId:number){
-    this.userService.joinGroup(groupId).subscribe(()=>
-      {this.alertService.success("You joined group");
-        this.reloadGame()},
-      ()=> this.alertService.error("Error while joining group"))
+
+  joinGroup(groupId: number) {
+    this.userService.joinGroup(groupId).subscribe(() => {
+        this.alertService.success('You joined group');
+        this.reloadGame()
+      },
+      () => this.alertService.error('Error while joining group'))
   }
 
   joinGroupMethod(groupId: number) {
     this.joinGroup(groupId);
-    // window.location.reload();
   }
 
-  deleteGroup(groupId:number){
+  deleteGroup(groupId: number) {
     this.groupRoomService.deleteGroup(groupId).subscribe(
       () => {
         this.alertService.success('You succesfully removed ur group');
@@ -76,11 +76,12 @@ export class DashboardComponent implements OnInit {
       },
       () => this.alertService.error('Error while removing group'));
   }
+
   checkIfAdmin() {
     this.userService.getUser().subscribe(
       data => {
         this.currentUser = data
-        if ( this.currentUser?.role.name === "ROLE_ADMIN") {
+        if (this.currentUser?.role.name === 'ROLE_ADMIN') {
           this.isAdmin = true;
         }
       }, () => {
@@ -88,64 +89,62 @@ export class DashboardComponent implements OnInit {
       }
     );
   }
+
   checkIfLoggedIn() {
     if (this.authService.getToken()) {
       return true;
-    }
-    else return false;
+    } else return false;
   }
+
   reloadGame() {
     this.chosenGame = this.categoryService.getGame();
     this.groupRoomService.getGroupsByGame(this.chosenGame?.name).subscribe(
-      (data:any) => this.groupRooms = data,
+      (data: any) => this.groupRooms = data,
       () => this.alertService.error('Error while getting groups')
     );
   }
 
-  public reloadByCategory(categoryId:number){
-    if(this.chosenRole == null && this.chosenCategory != null) {
-      this.groupRoomService.getGroupsByGameAndCategory(this.chosenGame.id, categoryId).subscribe(
+  public reloadByFilters() {
+    if (this.chosenRole == null && this.chosenCategory != null) {
+      this.groupRoomService.getGroupsByGameAndCategory(this.chosenGame.id, this.chosenCategory.id).subscribe(
         (data: any) => this.groupRooms = data,
         () => this.alertService.error('Error while getting groups')
       );
-    }else if(this.chosenRole != null && this.chosenCategory != null){
-      this.groupRoomService.getGroupsByGameCategoryRole(this.chosenGame.id,categoryId, this.chosenRole.id).subscribe(
+    } else if (this.chosenCategory == null && this.chosenRole != null) {
+      this.groupRoomService.getGroupsByGameAndRole(this.chosenGame.id, this.chosenRole.id).subscribe(
         (data: any) => this.groupRooms = data,
         () => this.alertService.error('Error while getting groups')
       );
-    }else{
+    } else if (this.chosenRole != null && this.chosenCategory != null) {
+      this.groupRoomService.getGroupsByGameCategoryRole(this.chosenGame.id, this.chosenCategory.id, this.chosenRole.id).subscribe(
+        (data: any) => this.groupRooms = data,
+        () => this.alertService.error('Error while getting groups')
+      );
+    } else {
       this.reloadGame();
     }
   }
-
-  public reloadByRole(roleId:number){
-    console.log(this.chosenCategory.name)
-      if(this.chosenCategory == null && this.chosenRole != null){
-        this.groupRoomService.getGroupsByGameAndRole(this.chosenGame.id, roleId).subscribe(
-          (data: any) => this.groupRooms = data,
-          () => this.alertService.error('Error while getting groups')
-        );
-      }else if(this.chosenRole != null && this.chosenCategory != null){
-        this.groupRoomService.getGroupsByGameCategoryRole(this.chosenGame.id,this.chosenCategory.id, this.chosenRole.id).subscribe(
-          (data: any) => this.groupRooms = data,
-          () => this.alertService.error('Error while getting groups')
-        );
-      }else{
-        this.reloadGame();
-      }
-  }
-
-  public changeCategory(e){
-    const temp = this.chosenGame.categories.map(a =>{ if(a.name === e.target.value){return a}else return
-    }).filter((value)=>{return value !== undefined});
+  public changeCategory(e) {
+    const temp = this.chosenGame.categories.map(a => {
+      if (a.name === e.target.value) {
+        return a
+      } else return
+    }).filter((value) => {
+      return value !== undefined
+    });
     this.chosenCategory = temp[0];
-    this.reloadByCategory(this.chosenCategory?.id)
+    this.reloadByFilters()
   }
 
-  public changeRole(e){
-    const temp = this.chosenGame.inGameRoles.map(a =>{ if(a.name === e.target.value){return a}else return
-    }).filter((value)=>{return value !== undefined});
+  public changeRole(e) {
+    const temp = this.chosenGame.inGameRoles.map(a => {
+      if (a.name === e.target.value) {
+        return a
+      } else return
+    }).filter((value) => {
+      return value !== undefined
+    });
     this.chosenRole = temp[0];
-    this.reloadByRole(this.chosenRole?.id);
+    this.reloadByFilters();
   }
 }
