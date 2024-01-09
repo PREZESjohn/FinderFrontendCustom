91b5d90b39aa9634314d48df480b951c919e76a0
Evistix <mephistakaiwan@gmail.com>
2024-01-09 22:55:34 +0100

    logout on wrong token

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,15 +3,16 @@ import {HttpClient} from '@angular/common/http';
 import {UserCredentials} from '../domain/UserCredentials';
 import {from, Observable} from 'rxjs';
 import {EmailDto} from '../domain/dto/EmailDto';
-
+import {Router} from "@angular/router";
 @Injectable({providedIn: 'root'})
 export class AuthService {
 
   baseUrl = 'http://localhost:8080/api/v1/auth';
   authUrl = 'http://localhost:8080/api/v1/auth/login';
   registerUrl = 'http://localhost:8080/api/v1/auth/new-account';
+  tryUrl = 'http://localhost:8080/api/v1/users/all';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private route: Router) {
   }
 
   login(login: string, password: string): Observable<any> {
@@ -40,13 +41,24 @@ export class AuthService {
     return sessionStorage.getItem('key');
   }
 
-  register(username: string,email:string, password: string): Observable<any> {
-    return this.http.post<TokenResponse>(this.registerUrl, {username,email, password});
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post<TokenResponse>(this.registerUrl, {username, email, password});
   }
 
+  checkIfTokenCorrect() {
+    let check = true
+      this.http.get(this.tryUrl).subscribe(data => {
+        check = true
+      }, () => {
+        check = false
+        this.logout()
+        this.route.navigate(['/login'])
+      })
+      return check
+  }
 
-  checkIfAdmin(){
-      const token = this.getToken();
+  checkIfAdmin() {
+    const token = this.getToken();
     let jwtData = token.split('.')[1]
     let decodedJwtJsonData = window.atob(jwtData)
     let decodedJwtData = JSON.parse(decodedJwtJsonData)
@@ -56,8 +68,9 @@ export class AuthService {
     return isAdmin == 'ROLE_ADMIN';
 
   }
-  checkToken(){
-    if(this.getToken()) {
+
+  checkToken() {
+    if (this.getToken()) {
       const token = this.getToken();
       try {
         let jwtData = token.split('.')[1]
@@ -66,31 +79,31 @@ export class AuthService {
 
         let role = decodedJwtData.role
 
-        return role == 'ROLE_USER' || role =="ROLE_ADMIN";
-      }catch(e){
+        return role == 'ROLE_USER' || role == "ROLE_ADMIN";
+      } catch (e) {
         this.logout()
         return false;
       }
-    }else{
+    } else {
       return false;
     }
 
   }
 
-  confirmDelete(token:string){
-    return this.http.get(this.baseUrl+"/deleteAccountConfirm?token="+token)
+  confirmDelete(token: string) {
+    return this.http.get(this.baseUrl + "/deleteAccountConfirm?token=" + token)
   }
 
-  confirmRegister(token:string){
-    return this.http.get(this.baseUrl+"/confirmAccountRegister?token="+token)
+  confirmRegister(token: string) {
+    return this.http.get(this.baseUrl + "/confirmAccountRegister?token=" + token)
   }
 
-  changeEmail(email:EmailDto){
-    return this.http.patch(this.baseUrl+"/emailChange",email)
+  changeEmail(email: EmailDto) {
+    return this.http.patch(this.baseUrl + "/emailChange", email)
   }
 
-  confirmEmailChange(token:string){
-    return this.http.get(this.baseUrl + "/confirmEmailChange?token="+token)
+  confirmEmailChange(token: string) {
+    return this.http.get(this.baseUrl + "/confirmEmailChange?token=" + token)
   }
 
 
